c3ad27cdeeee4c79fd28c396f4a8e706d00c8973
Clement <clement.bisaillon@hotmail.com>
2019-10-11 17:38:15 -0400

    initial commit

---
+++ src/clem/DataStructures.kt
@@ -0,0 +1,12 @@
+package clem
+
+import clem.datastructures.CArrayList
+
+/**
+ * Entry point of datastructures I developped
+ * @Author: Clement Bisaillon
+ * @Date: 2019-10-11
+ */
+fun main(args: Array<String>){
+    val list = CArrayList()
+}
---
+++ src/clem/datastructures/ArrayList.kt
@@ -0,0 +1,7 @@
+package clem.datastructures
+
+class CArrayList(){
+    fun a(){
+        println("a")
+    }
+}
