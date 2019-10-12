243178bfc7cac825662e89194a29f8ed387e73d9
Clement <clement.bisaillon@hotmail.com>
2019-10-12 16:23:20 -0400

    Start of array list

--- src/clem/DataStructures.kt
+++ src/clem/DataStructures.kt
@@ -3,10 +3,17 @@ package clem
 import clem.datastructures.CArrayList
 
 /**
- * Entry point of datastructures I developped
+ * Entry point of data structures I developped
  * @Author: Clement Bisaillon
  * @Date: 2019-10-11
  */
-fun main(args: Array<String>){
-    val list = CArrayList()
+fun main(){
+    val list = CArrayList<Int>()
+
+    list.add(1)
+    list.add(2)
+    list.add(6)
+
+    println(list.toString())
+    println("Size: ${list.size}")
 }
--- src/clem/datastructures/ArrayList.kt
+++
@@ -1,7 +0,0 @@
-package clem.datastructures
-
-class CArrayList(){
-    fun a(){
-        println("a")
-    }
-}
